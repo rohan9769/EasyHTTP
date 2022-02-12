7eb5d44d105bc5dae9945d3360bf608ba86c6a6b
Rohan Nayak <rohannayak2071@gmail.com>
2022-02-12 14:50:23 -0500

    added PUT Method for API Request

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
 const http = new easyHTTP;
 
+// GET METHOD
+
 // Get Posts
-const posts = http.get('https://jsonplaceholder.typicode.com/posts',
-    function(err,posts){
-        if(err){
-            console.log(err)
-        }
-        else{
-            console.log(posts)
-        }
+// const posts = http.get('https://jsonplaceholder.typicode.com/posts',
+//     function(err,posts){
+//         if(err){
+//             console.log(err)
+//         }
+//         else{
+//             console.log(posts)
+//         }
+//     }
+// )
+
+// Get Single Post
+// const post = http.get('https://jsonplaceholder.typicode.com/posts/1',
+//     function(err,post){
+//         if(err){
+//             console.log(err)
+//         }
+//         else{
+//             console.log(post)
+//         }
+//     }
+// )
+
+// POST METHOD
+// Creating data
+
+const data = {
+    title:'Custom Post',
+    body:'This is a custom post by Ron'
+};
+
+// Creating Post
+http.post('https://jsonplaceholder.typicode.com/posts',data,function(err,post){
+    if(err){
+        console.log(err)
     }
-)
+    else{
+        console.log(post)
+    }
+})
+
+
--- easyhttp.js
+++ easyhttp.js
@@ -20,6 +20,17 @@ easyHTTP.prototype.get = function(url,callback){
 }
 
 // POST Method - Making an HTTP POST Request
+easyHTTP.prototype.post = function(url,data,callback){
+    this.http.open('POST',url,true)
+
+    this.http.setRequestHeader('Content-type','application/json')
+    let self = this;
+    this.http.onload = function(){
+            callback(null,self.http.responseText)
+    }
+
+    this.http.send(JSON.stringify(data))
+}
 
 // PUT Method - Making an HTTP PUT Request
 
